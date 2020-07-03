63583197d3fd218fafcc62b7f78019dd76641cb7
Ben LaVerriere <ben.laverriere@bookbub.com>
2020-07-02 20:15:59 -0400

    Add experimental color transition.
    This will probably be replaced by a shader.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import VideoContext from "videocontext";
+//import colorwheel from "colorwheel";
 
 var canvas = document.getElementById("canvas");
-const { MONOCHROME: monochromeEffect } = VideoContext.DEFINITIONS;
+const { CROSSFADE, MONOCHROME } = VideoContext.DEFINITIONS;
 
 function rgbToOutputMix(r, g, b) {
   return [r / 255.0, g / 255.0, b / 255.0];
 }
 
 function colorOverlayEffectNode(r, g, b) {
-  const effectNode = videoCtx.effect(monochromeEffect);
+  const effectNode = videoCtx.effect(MONOCHROME);
 
   // balance all inputs so we're doing a "flat" monochrome conversion
   effectNode.inputMix = [1.0 / 3, 1.0 / 3, 1.0 / 3];
@@ -17,6 +18,15 @@ function colorOverlayEffectNode(r, g, b) {
   effectNode.outputMix = rgbToOutputMix(r, g, b);
 
   return effectNode;
+}
+
+function crossfadeNode() {
+  var crossfadeEffect = videoCtx.transition(CROSSFADE);
+
+  //Setup the transition. This will change the "mix" property of the cross-fade node from 0.0 to 1.0.
+  //Transision mix value from 0.0 to 1.0 at time=8 over a period of 2 seconds to time=10.
+  crossfadeEffect.transition(8.0, 10.0, 0.0, 1.0, "mix");
+  return crossfadeEffect;
 }
 
 var videoCtx = new VideoContext(canvas);
@@ -29,10 +39,21 @@ var videoNode = videoCtx.video(
 videoNode.connect(videoCtx.destination);
 videoNode.start(0);
 
-const gelEffectNode = colorOverlayEffectNode(252, 115, 3);
+const orangeGel = colorOverlayEffectNode(252, 115, 3);
+const redGel = colorOverlayEffectNode(153, 23, 23);
+const greenGel = colorOverlayEffectNode(17, 194, 97);
+const blueGel = colorOverlayEffectNode(64, 110, 201);
+const colorwheel = crossfadeNode();
 
 // Set up the processing chain.
-videoNode.connect(gelEffectNode);
-gelEffectNode.connect(videoCtx.destination);
+videoNode.connect(orangeGel);
+videoNode.connect(redGel);
+videoNode.connect(greenGel);
+videoNode.connect(blueGel);
+
+orangeGel.connect(colorwheel);
+redGel.connect(colorwheel);
+
+colorwheel.connect(videoCtx.destination);
 
 videoCtx.play();
